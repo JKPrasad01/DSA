b7e0242a5ec82e4d40d4d56923492cc10f76ef09
JK Prasad <prasadjk2241@gmail.com>
2025-09-01 10:56:23 +0530

    dfs problem

--- src/trees/bsf/DFS.java
+++ src/trees/bsf/DFS.java
@@ -28,5 +28,59 @@ public class DFS {
 
     }
 
+    // check the tree is balanced
+    public boolean isBalanced(TreeNode root) {
+        if (root==null)return true;
+        return checkBalanced(root)!=-1;
+    }
 
+    private int checkBalanced(TreeNode node) {
+        if( node==null)return 0;
+
+        int left=checkBalanced(node.left);
+        if(left==-1)return -1;
+        int right=checkBalanced(node.right);
+        if(right==-1)return -1;
+
+        if(Math.abs(left-right)>1)return -1;
+
+        return Math.max(left,right)+1;
+    }
+
+    int diameter=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root==null)return 0;
+
+         diameter(root);
+         return diameter;
+    }
+
+    private int diameter(TreeNode node) {
+        if (node==null)return 0;
+
+        int left=diameter(node.left);
+        int right=diameter(node.right);
+        diameter=Math.max(diameter,left+right);
+
+        return Math.max(left,right)+1;
+    }
+
+
+    int max=0;
+    public int maxPathSum(TreeNode root) {
+        if(root==null)return 0;
+
+        max=root.val;
+        maxSum(root);
+        return max;
+    }
+
+    private int maxSum(TreeNode node) {
+        if(node==null)return 0;
+        int l=Math.max(0,maxSum(node.left));
+        int r=Math.max(0,maxSum(node.right));
+
+        max=Math.max(max,l+r+node.val);
+        return Math.max(l,r)+node.val;
+    }
 }
