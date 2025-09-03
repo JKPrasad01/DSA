6900a8e4df85782de684f6b0fa8c0a5ea9b98b3a
jk.prasad <prasadjk2241@gmail.com>
2025-09-03 22:08:48 +0530

    solve linkedList problem   isPalindrome , mergeTwoLists

--- src/d/linkedlist/LLProblems.java
+++ src/d/linkedlist/LLProblems.java
@@ -33,4 +33,76 @@ public class LLProblems {
         }
         return s;
     }
+
+
+    public boolean isPalindrome(ListNode head) {
+        if(head==null)return true;
+        ListNode f=head;
+        ListNode s=head;
+
+        while (f!=null && f.next!=null){
+            f=f.next.next;
+            s=s.next;
+        }
+
+        ListNode firstHalf=head;
+        ListNode secondHalf=reverseListNode(s);
+
+        while (secondHalf!=null){
+            if(firstHalf.val!=secondHalf.val){
+                return false;
+            }
+            firstHalf=firstHalf.next;
+            secondHalf=secondHalf.next;
+        }
+        return true;
+    }
+
+    private ListNode reverseListNode(ListNode cur) {
+        if(cur==null)return null;
+
+        ListNode prev=null;
+        while (cur!=null){
+            ListNode N=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=N;
+        }
+        return prev;
+    }
+
+//
+//    Input: list1 = [1,2,4], list2 = [1,3,4]
+//    Output: [1,1,2,3,4,4]
+
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy=new ListNode();
+        ListNode cur=dummy;
+
+        while (list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                cur.next=list1;
+                list1=list1.next;
+            }
+            else{
+                cur.next=list2;
+                list2=list2.next;
+            }
+            cur=cur.next;
+        }
+
+        while (list1!=null){
+            cur.next=list1;
+            list1=list1.next;
+            cur=cur.next;
+        }
+
+        while (list2!=null){
+            cur.next=list2;
+            list2=list2.next;
+            cur=cur.next;
+        }
+        return dummy.next;
+    }
 }
