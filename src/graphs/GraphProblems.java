6b4f260691716ac48475dbbe8c55f8044428f6ac
JK Prasad <prasadjk2241@gmail.com>
2026-01-12 10:12:54 +0530

    add Number of provinces

--- src/graphs/GraphProblems.java
+++ src/graphs/GraphProblems.java
@@ -62,6 +62,32 @@ public class GraphProblems {
     }
 
 
+    public int findCircleNum(int[][] isConnected) {
+
+        boolean[] visited = new boolean[isConnected.length];
+        int count =0;
+
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                dfs(i, visited,isConnected);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(int i,boolean[] visited,int[][] isConnected){
+        visited[i]=true;
+
+        for(int j=0;j<isConnected[i].length;j++){
+            if(isConnected[i][j]==1 && !visited[j]){
+                dfs(j,visited,isConnected);
+            }
+        }
+    }
+
+
 
 
 
