714c96c515128412e5e59aab47053458d67ea1c6
JK Prasad <prasadjk2241@gmail.com>
2026-01-31 10:29:02 +0530

    graphs problem 3.count of Islands

--- src/graphs/GraphProblems.java
+++ src/graphs/GraphProblems.java
@@ -136,4 +136,35 @@ public class GraphProblems {
         dfsFloodFill(image,color,newColor,i,j-1);
         dfsFloodFill(image,color,newColor,i,j+1);
     }
+
+    //this is for vertical , horizontal and diagonal elements
+    public int countIslands(char[][] grid) {
+
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for (int j=0;j<grid[0].length;j++){
+                count++;
+                dfsCountIslands(grid,i,j);
+            }
+        }
+
+        return count;
+    }
+
+    private void dfsCountIslands(char[][] grid, int i, int j) {
+
+        if(i<0 || i> grid.length-1 || j<0 || j>grid[0].length-1 || grid[i][j]!='L')return;
+        grid[i][j]='W';
+
+        dfsCountIslands(grid,i,j+1);
+        dfsCountIslands(grid,i,j-1);
+        dfsCountIslands(grid,i+1,j);
+        dfsCountIslands(grid,i-1,j);
+
+        dfsCountIslands(grid,i-1,j-1);
+        dfsCountIslands(grid,i-1,j+1);
+
+        dfsCountIslands(grid,i+1,j-1);
+        dfsCountIslands(grid,i+1,j+1);
+    }
 }
